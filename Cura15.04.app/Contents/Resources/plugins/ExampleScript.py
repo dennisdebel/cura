12ab9e13d37122a08e5d4f91105596b7703c22b8
dennisdebel <dennisdebel@gmail.com>
2016-05-05 13:49:53 +0200

    why uncommited changes???
    no clue

---
+++ Cura15.04.app/Contents/Resources/plugins/ExampleScript.py
@@ -0,0 +1,43 @@
+# Copyright (c) 2015 Jaime van Kessel, Ultimaker B.V.
+# The PostProcessingPlugin is released under the terms of the AGPLv3 or higher.
+from ..Script import Script
+
+class ExampleScript(Script):
+    def __init__(self):
+        super().__init__()
+        
+    def getSettingData(self):
+        return { 
+            "label":"Example script",
+            "key": "ExampleScript",
+            "settings": 
+            {
+                "test": 
+                {
+                    "label": "Test",
+                    "description": "None",
+                    "unit": "mm",
+                    "type": "float",
+                    "default": 0.5,
+                    "min_value": "0",
+                    "min_value_warning": "0.1",
+                    "max_value_warning": "1",
+                    "visible": True
+                },
+                "derp":
+                {
+                    "label": "zomg",
+                    "description": "afgasgfgasfgasf",
+                    "unit": "mm",
+                    "type": "float",
+                    "default": 0.5,
+                    "min_value": "0",
+                    "min_value_warning": "0.1",
+                    "max_value_warning": "1",
+                    "visible": True
+                }
+            }
+        }
+    
+    def execute(self, data):
+        return data
